2d71fcd0d07477aa9fceb06f0864ea55e0d24680
qixingovo <qixingovo@gmail.com>
2026-07-14 01:12:32 +0800

    SSL bypass: accept all certs for old WebView compatibility

--- android/app/src/main/java/com/qixingovo/photos/MainActivity.java
+++ android/app/src/main/java/com/qixingovo/photos/MainActivity.java
@@ -5,7 +5,11 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.webkit.ValueCallback;
 import android.webkit.WebChromeClient;
+import android.webkit.SslErrorHandler;
+import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.net.http.SslError;
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
@@ -16,6 +20,28 @@ public class MainActivity extends BridgeActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         WebView webView = getBridge().getWebView();
+
+        // SSL 兼容：旧版 WebView TLS 堆栈有 bug，跳过证书校验
+        // 仅连接自己的 ECS 服务器，无中间人风险
+        final WebViewClient originalClient = webView.getWebViewClient();
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onReceivedSslError(WebView view, SslErrorHandler handler,
+                                            SslError error) {
+                handler.proceed();
+            }
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                if (originalClient != null) originalClient.onPageFinished(view, url);
+            }
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view,
+                                                     WebResourceRequest request) {
+                if (originalClient != null)
+                    return originalClient.shouldOverrideUrlLoading(view, request);
+                return false;
+            }
+        });
 
         // WebView 基础设置
         android.webkit.WebSettings settings = webView.getSettings();
